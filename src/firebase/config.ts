528dc46dc40bc64827d3cafefc305a1404cf82eb
Siva shankar Vemulakonda <vemulakondasivashankar@gmail.com>
2025-10-18 08:05:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4574765025-4565d",
+  "appId": "1:993256933907:web:f561bf32b3748b9428cde6",
+  "apiKey": "AIzaSyANdQUykOsCBQdrfRYrDrDyGtztAZIgVDU",
+  "authDomain": "studio-4574765025-4565d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "993256933907"
+};
